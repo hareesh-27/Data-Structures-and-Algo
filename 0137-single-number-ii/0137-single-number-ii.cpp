1cdaa3ef334670eab8677e771d4fbb529d9d8f65
hareesh-27 <hareesh27d@gmail.com>
2026-02-28 19:27:10 +0530

    Time: 3 ms (32.82%), Space: 13.3 MB (64.42%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i=i+3)
+       int ans=0;
+       for(int bit_index=0;bit_index<32;bit_index++)        //since 32 bits
+       {
+        int cnt=0;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]!=nums[i-1]) return nums[i-1];
+            if((nums[i] & (1<<bit_index)) !=0) cnt+=1;         //check ith bit is set or not
         }
-        
-        return nums[n-1];
+        if(cnt%3 == 1) ans=ans | (1<<bit_index);             // ans is initially 0000                   
+       }
+       return ans;
     }
 };
