db8de8410a6e7259d18937bf79330a12f6c006c1
hareesh-27 <hareesh27d@gmail.com>
2026-03-06 14:06:43 +0530

    Create code.cpp

---
+++ DSA/Arrays/Majority element I/code.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt=0; int el; int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(cnt==0)
+            {
+                el=nums[i];
+                cnt++;
+            }
+            else if(nums[i]!=el) cnt--;
+
+            else if(nums[i]==el) cnt++;
+        }
+        return el;
+    }
+};
