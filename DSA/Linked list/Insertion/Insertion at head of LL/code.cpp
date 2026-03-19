34b182f034c4a7fbc6bfcac0e59aaf990a7779d3
hareesh-27 <hareesh27d@gmail.com>
2026-03-19 17:19:17 +0530

    Create code.cpp

---
+++ DSA/Linked list/Insertion/Insertion at head of LL/code.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    public:
+        ListNode* insertAtHead(ListNode* &head, int X) {
+            ListNode* newnode = new ListNode(X);
+            newnode->next=head;
+            head=newnode;
+            return head;
+        }
+};
